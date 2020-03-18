34eaf544c63bde90dbb8fb87f6e50d5f5b178820
markziemann <mark.ziemann@gmail.com>
2020-03-18 22:35:46 +1100

    got ground truth gene sets working

--- scSIM_muscat.R
+++ scSIM_muscat.R
@@ -34,6 +34,7 @@ sce$id <- paste0(sce$stim, sce$ind)
                 sample_id = "id",    # sample IDs (ctrl/stim.1234)
                 drop = TRUE))        # drop all other colData columns
 
+
 nk <- length(kids <- levels(sce$cluster_id))
 ns <- length(sids <- levels(sce$sample_id))
 names(kids) <- kids; names(sids) <- sids
@@ -47,6 +48,7 @@ names(kids) <- kids; names(sids) <- sids
 # perform the simulation using Muscat simulation functions
 ##########################################################
 # prep. SCE for simulation
+# need to see why we are losing some cell types at this step.
 sce <- prepSim(sce, verbose = TRUE)
 
 # simulate data with 10% DE
@@ -67,74 +69,126 @@ pb <- aggregateData(sim,
                     by = c("cluster_id", "sample_id"))
 
 res <- pbDS(pb, verbose = TRUE)
-# tbl <- res$table$B
+
+tbl <- res$table$B
 
 
 for (CELL_ID in names(tbl)) {
- 
-    mytbl <- metadata(sim)$gene_info[which(metadata(sim)$gene_info$cluster_id == CELL_ID),]
-    genes_up <- mytbl[which(mytbl$logFC>0),1]
-    genes_dn <- mytbl[which(mytbl$logFC<0),1]
-
+  
+  mytbl <- metadata(sim)$gene_info[which(metadata(sim)$gene_info$cluster_id == CELL_ID),]
+  genes_up <- mytbl[which(mytbl$logFC>0),1]
+  genes_dn <- mytbl[which(mytbl$logFC<0),1]
+  
 }
+
+##########################################################
+# Extract the ground truth DEGs
+##########################################################
+y <- vector("list", length(unique(gi$cluster_id)))
+
+n=0
+for ( CELL_ID in unique(gi$cluster_id) ) {
+  n=n+1
+  y1 <- subset(gi,logFC>0 & cluster_id==CELL_ID)$gene
+  y2 <- subset(gi,logFC<0 & cluster_id==CELL_ID)$gene
+
+  yy <- list(y1,y2)
+  names(yy)=c("up","dn")
+
+  y[n] <- list(yy)
+}
+names(y) <- unique(gi$cluster_id)
 
 ##########################################################
 # create random gene sets
 ##########################################################
-randomGeneSets<-function(a){
-  gsets<-sapply( rep(50,1000) , function(x) {list(as.character(sample(a,x))) } )
-  names(gsets)<-stri_rand_strings(length(gsets), 15, pattern = "[A-Za-z]")
+randomGeneSets <- function(a) {
+  gsets<-sapply( rep(50,1000) , function(x) {
+    list(as.character(sample(a,x))) 
+  } )
+  names(gsets) <- stri_rand_strings(length(gsets), 15, pattern = "[A-Za-z]")
   gsets
 }
 
 library(stringi)
 mygenes <- unique(gi$gene)
-gsets <- randomGeneSets(mygenes)
+randomSets <- randomGeneSets(mygenes)
 
-# TODO here need to set the ground truth gene sets
-y <- list()
+##########################################################
+# create gene sets with a fraction that are DE
+##########################################################
+gs <- vector("list", length(unique(gi$cluster_id)))
+
+# gene sets will consist of nde #genes that are de and nee #genes that are not de (ee)
+nde=20
+nee=30
+nsets=10
+
 n=0
 for ( CELL_ID in unique(gi$cluster_id) ) {
   n=n+1
-  y1<-list(subset(gi,logFC>0 & cluster_id==CELL_ID)$gene)
-  y2<-list(subset(gi,logFC<0 & cluster_id==CELL_ID)$gene)
-  #y[n]<-c(y1,y2)
-  y[n] <- y1
+  # first make sets of upregulated genes
+  i=0
+  up <- vector("list", nsets)
+  for ( i in seq(nsets) ) {
+    deg <- sample(subset(gi,logFC>0 & cluster_id==CELL_ID)$gene,nde)
+    ndeg <- sample(unique(gi$gene),nee)
+    mygs <- union(deg,ndeg)
+    up[i] <- list(mygs)
+    i=i+1
   }
+  names(up) <- stri_rand_strings(length(up), 15, pattern = "[A-Za-z]")
 
+  # now make sets of downregulated genes
+  i=0
+  dn <- vector("list", nsets)
+  for ( i in seq(nsets) ) {
+    deg <- sample(subset(gi,logFC<0 & cluster_id==CELL_ID)$gene,nde)
+    ndeg <- sample(unique(gi$gene),nee)
+    mygs <- union(deg,ndeg)
+    dn[i] <- list(mygs)
+    i=i+1  
+  }
+  names(dn) <- stri_rand_strings(length(up), 15, pattern = "[A-Za-z]")
 
-#getDEsets<-function(CELL_ID) {
-#    y1<-list(subset(gi,logFC>0 & cluster_id=="NK cells")$gene)
-#  y2
-#  }  
+  cell<-list("up"=up,"dn"=dn)
 
+  gs[n] <- list(cell)
+
+}
+names(gs) <- unique(gi$cluster_id)
+
+# flatten the list
+desets <- unlist(unlist(gs,recursive=FALSE),recursive=FALSE)
+
+#########################################################
+# combine DE genesets with some random genesets
+#########################################################
+
+randomGeneSets <- function(a) {
+  gsets<-sapply( rep(50,920) , function(x) {
+    list(as.character(sample(a,x)))
+  } )
+  names(gsets) <- stri_rand_strings(length(gsets), 15, pattern = "[A-Za-z]")
+  gsets
+}
+
+library(stringi)
+mygenes <- unique(gi$gene)
+randomSets <- randomGeneSets(mygenes)
+
+# merge the random ones and the DE sets
+mysets <- c(desets,randomSets)
 
 ##########################################################
 # analyse with mitch
 ##########################################################
 
+
 x <- mitch_import(res$table$B, DEtype = "muscat", geneIDcol = "gene")
 
-mitch_res <- mitch_calc(x,gsets)
+mitch_res <- mitch_calc(x,mysets)
 
 head(mitch_res$enrichment_result)
 
-#todo
-#- process sim with muscat
-t(head(assay(pb)))
-
-metadata(sim)
-
-str(gi)
-head(gi,30)
-dim(gi)
-head(gi$category,30)
-table(gi$category)
-table(gi$cluster_id)
-summary(gi$sim_disp)
-str(sim)
-
-table(sim$sample_id)
-
-?metadata
-str(metadata(sim))
+#todo need to perform p-adjust for each cell type and then calculate the sensitivity, specificity and F1 score
